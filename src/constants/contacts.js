51daf996d13f13be2817f24381c2206d50489be4
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-05-06 13:27:11 +0300

    thirth commit

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB = 'src/db/db.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
+console.log(fileName);
+
+export const PATH_DB = path.join(dirName, '..', 'db', 'db.json');
